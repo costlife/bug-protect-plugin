01d3b7dc545e16a5ef5d5bc6298d9bd39a525b26
Kevin Liu <costlife@foxmail.com>
2018-07-31 22:27:01 +0800

    first init

---
+++ index.js
@@ -0,0 +1,31 @@
+const pluginName = 'BugProtectPlugin';
+
+class BugProtectPlugin {
+  apply(compiler) {
+    compiler.hooks.run.tap(pluginName, compilation => {
+      console.log('                    _ooOoo_')
+      console.log('                   o8888888o')
+      console.log('                   88" . "88')
+      console.log('                   (| -_- |)')
+      console.log('                    O\\ = /O')
+      console.log('                ____/`---`\\____')
+      console.log('              .   \' \\| |// \'   .')
+      console.log('               / \\||| : |||// \\')
+      console.log('             / _||||| -:- |||||- \\')
+      console.log('               | | \\\ - /// | |')
+      console.log("             | \\_| ''\\---/'' | |")
+      console.log('              \\ .-\\__ `-` ___/-. /')
+      console.log('           ___`. . /--.--\\ `. . __')
+      console.log('        .""  < `.___\\_<|>_/___.` >\\  "".')
+      console.log('       | | : `- \\`.;`\\ _ /`;.`/ - ` : | |')
+      console.log('         \\ \\ `-. \\_ __\\ /__ _/ .-` / /')
+      console.log(' ======`-.____`-.___\\_____/___.-`____.-`======')
+      console.log('                    `=---=`')
+      console.log('')
+      console.log(' .............................................')
+      console.log(' .............佛祖保佑....永无BUG.............')
+      console.log('')
+    });
+  }
+}
+module.exports = BugProtectPlugin
